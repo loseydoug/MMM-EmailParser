e00b1f11f07a94deee6f9151a16911dbd16131ea
loseydoug <losey.doug@gmail.com>
2019-11-22 18:46:54 -0500

    add shutdown button

--- node_helper.js
+++ node_helper.js
@@ -7,12 +7,14 @@
 
 const NodeHelper = require("node_helper");
 const Fetcher = require("./fetcher.js");
+const moment = require('moment');
 
 module.exports = NodeHelper.create({
 	// Subclass start method.
 	start: function() {
 		console.log("Starting helper module: " + this.name);
 		this.fetchers = [];
+		this.buttonClicks = 0;
 	},
 
 	// Subclass socketNotificationReceived received.
@@ -20,6 +22,19 @@ module.exports = NodeHelper.create({
 		if (notification === "ADD_FEED") {
 			this.createFetcher(payload.account, payload.config);
 			return;
+		}
+
+		if (notification === "EMAIL_READ") {
+			if (this.buttonClicks === 0) {
+				this.buttonClicks++;
+				const startTime = moment();
+			} else if (this.buttonClicks > 3) {
+				this.buttonClicks++;
+			} else {
+				const endTime = moment();
+				console.log(moment.duration(endTime.diff(startTime).as('seconds'));
+				moment.duration(endTime.diff(startTime).as('seconds') < 3 ? exec('sudo shutdown -h now, null') : this.buttonClicks = 0;
+			}
 		}
 	},
 
@@ -37,7 +52,7 @@ module.exports = NodeHelper.create({
 		const reloadInterval = account.reloadInterval || config.reloadInterval || 5 * 60 * 1000;
 		if (typeof self.fetchers[account.user] === "undefined") {
 			console.log("Create new news fetcher for account: " + account.user + " - Interval: " + reloadInterval);
-	
+
 			fetcher = new Fetcher(reloadInterval, encoding, account);
 
 			fetcher.onReceive(function(fetcher) {
@@ -53,7 +68,7 @@ module.exports = NodeHelper.create({
 			self.fetchers.push(fetcher);
 
 		} else {
-			console.log("Use existing news fetcher for url: " + url);
+			console.log("Use existing email fetcher for account: " + account.user);
 			fetcher = self.fetchers[url];
 			fetcher.setReloadInterval(reloadInterval);
 			fetcher.broadcastItems();
